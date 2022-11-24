51987c493d2d2c527452019eaad7f4742bfedf0b
Micka <lucasmichaeli95@gmail.com>
2022-11-24 10:48:14 +0100

    Use startsession function by saga
    First session run fetch get to the public session
    afterwards we return the session and add it to the state

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,6 +1,5 @@
 import * as sagaSession from './sagaSession';
 
 export default function* rootSaga() {
-    yield sagaSession.fetchReceiveSession()
     yield sagaSession.fetchStartSession()
 };
--- src/sagas/sagaSession.js
+++ src/sagas/sagaSession.js
@@ -1,23 +1,13 @@
-import { call, takeEvery } from "redux-saga";
-import { put } from "redux-saga/effects";
+import { put, call } from "redux-saga/effects";
 import { RECEIVE_SESSION } from "../actions";
 import { startSession } from "../api";
 
 export function* fetchStartSession(action){
     try{
-        yield call(startSession);
-        yield put({type: RECEIVE_SESSION});
+        const session = yield call(startSession);
+        yield put({type: RECEIVE_SESSION, session: session} );
     }
     catch(error){
         console.log(error);
     };
-};
-
-export function* fetchReceiveSession(state = { session: {} }, action){
-    try{
-        return { ...state, session: action.session }
-    }
-    catch(error){
-        console.log(error);
-    }
 };
