25362a1e5c35a5bb6fade097398152fcc7ce3bef
Micka <lucasmichaeli95@gmail.com>
2022-11-28 17:10:23 +0100

    into saga beer chaneg fetchingBeerValue in comment because no need now
    it will be helpful when keep state in memorize because at this moment no
    need to reload beers each time

--- src/sagas/sagaBeers.js
+++ src/sagas/sagaBeers.js
@@ -6,12 +6,12 @@ import { getBeers } from "../api";
 
 export default function* fetchBeersSaga(){
   try{
-    const fetchingBeersValue = yield select(isFetchingBeers);
+    // const fetchingBeersValue = yield select(isFetchingBeers);
     const settingsInfo = yield select(settings);
 
-    if (fetchingBeersValue){
-      return
-    }
+    // if (fetchingBeersValue){
+    //   return
+    // }
 
     yield put(fetchingBeers(true));
     const beers = yield call(getBeers, settingsInfo.session.id);
