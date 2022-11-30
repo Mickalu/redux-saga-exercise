80c388d9d712636e37ab1c7062a1cd78b647483f
Micka <lucasmichaeli95@gmail.com>
2022-11-30 14:16:11 +0100

    Create a custome render for tests
    - Need custome render because or else need to add Provider and store
      into each test file

--- src/__tests__/customeMethodRTL.js
+++ src/__tests__/customeMethodRTL.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { screen, render } from "@testing-library/react";
 import { Provider } from "react-redux";
 
@@ -6,3 +7,13 @@ import store from "../store";
 export const queryAllByTestId = (element) => {
     return screen.queryAllByTestId(element)
 }
+
+const renderCustome = (element) => {
+    render(
+        <Provider store={store} >
+            {element}
+        </Provider>
+    )
+};
+
+export {renderCustome as render};
--- src/components/Navigations/Menus/__tests__/BeersLiked.spec.js
+++ src/components/Navigations/Menus/__tests__/BeersLiked.spec.js
@@ -1,22 +1,18 @@
 import React from "react";
 
 import { expect } from "chai";
-import {  render } from "@testing-library/react";
+
 import { initListBeers, propsLikedBeerEmpty, propsLikedBeersNotEmpty } from "../../../../__tests__/initValues";
 import BeersLiked from "../Beersliked";
-import { Provider } from "react-redux";
-import store from "../../../../store";
-import { queryAllByTestId } from "../../../../__tests__/customeMethodRTL";
+import { queryAllByTestId, render} from "../../../../__tests__/customeMethodRTL";
 
 describe("BeersLiked component should display all liked beer title", () => {
     it("Should display only a div if liked beers array is empty", () => {
         render (
-            <Provider store={store} >
-                <BeersLiked
-                    likes={{ data: propsLikedBeerEmpty }}
-                    beers={ initListBeers }
-                />
-            </Provider>
+            <BeersLiked
+                likes={ propsLikedBeerEmpty }
+                beers={ initListBeers }
+            />
         );
 
         expect(queryAllByTestId("beer-title").length).to.be.equal(0);
@@ -24,12 +20,10 @@ describe("BeersLiked component should display all liked beer title", () => {
 
     it("Should return same number of beer-title than number of beers liked", () => {
         render(
-            <Provider store={store} >
             <BeersLiked
                 likes={{ data: propsLikedBeersNotEmpty }}
                 beers={ initListBeers }
             />
-        </Provider>
         );
 
         expect(queryAllByTestId("beer-title").length).to.be.equal(13);
