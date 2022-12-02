6127c8e5f9ff43f5c32a641e5b3f26e7cdd71141
Micka <lucasmichaeli95@gmail.com>
2022-12-02 16:45:34 +0100

    11 - in rootSaga from saga/index change multiple yield for yield all

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,4 +1,4 @@
-import { takeLeading } from 'redux-saga/effects';
+import { takeLeading, all } from 'redux-saga/effects';
 
 import { START_SESSION, FETCH_BEERS, ADD_LIKE, NEXT_BEER } from '../actions';
 import * as likeSaga from "./addBeerLikeSaga";
@@ -6,8 +6,10 @@ import fetchBeersSaga from "./fetchBeersSaga";
 import startSessionSaga from './startSessionSaga';
 
 export default function* rootSaga() {
-  yield takeLeading(START_SESSION, startSessionSaga)
-  yield takeLeading(FETCH_BEERS, fetchBeersSaga)
-  yield takeLeading(ADD_LIKE, likeSaga.addLikesSaga)
-  yield takeLeading(NEXT_BEER, likeSaga.nextBeerSaga)
+  yield all([
+    takeLeading(START_SESSION, startSessionSaga),
+    takeLeading(FETCH_BEERS, fetchBeersSaga),
+    takeLeading(ADD_LIKE, likeSaga.addLikesSaga),
+    takeLeading(NEXT_BEER, likeSaga.nextBeerSaga),
+  ]);
 };
